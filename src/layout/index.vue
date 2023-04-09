96abb19b5334cd53acd8e06ded0db0ec72ac6d48
王榨菜 <wongself@gmail.com>
2023-04-09 20:08:58 +0800

    fix: dynamic height (need more test)

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <n-layout has-sider class="z-40 w-full h-screen transition" :class="{ 'pt-14': isMobile }">
+  <n-layout has-sider class="z-40 w-full h-[100dvh] transition" :class="{ 'pt-14': isMobile }">
     <Header v-if="isMobile" class="z-50" />
     <n-layout-sider
       bordered
